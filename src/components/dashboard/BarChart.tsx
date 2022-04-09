ad9dcce900ae321418a81c194aff8a0d3199e541
mhaecal <haecal.dk@gmail.com>
2022-04-09 21:47:55 +0700

    chore: install `react-apexcharts` & add `BarChart`

---
+++ src/components/dashboard/BarChart.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import Chart from 'react-apexcharts'
+import Box from '../Box'
+import Card from '../Card'
+
+function BarChart() {
+  const [data, setData] = useState({
+    options: {
+      chart: {
+        toolbar: {
+          show: false,
+        },
+        parentHeightOffset: 0,
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      colors: ['#38BDF8'],
+      fill: {
+        opacity: 0.8,
+      },
+      labels: ['1 Mar', '2 Mar', '3 Mar', '4 Mar', '5 Mar', '6 Mar', '7 Mar', '8 Mar'],
+    },
+    series: [
+      {
+        name: 'Visitor',
+        data: [25, 40, 45, 60, 76, 60, 70, 91],
+      },
+    ],
+  })
+
+  return (
+    <Box className="grid md:grid-cols-2 md:gap-2 lg:gap-10">
+      <Card title="Visitor">
+        <Chart options={data.options} series={data.series} type="bar" width="100%" />
+      </Card>
+    </Box>
+  )
+}
+
+export default BarChart
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,22 +2,18 @@ import React from 'react'
 import * as Icon from 'react-feather'
 import InfoCardContainer from '../components/dashboard/InfoCardContainer'
 import InfoCard from '../components/dashboard/InfoCard'
-import Card from '../components/Card'
+import BarChart from '../components/dashboard/BarChart'
 
 function Dashboard() {
   return (
     <>
       <InfoCardContainer>
-        <>
-          <InfoCard title="Income" value="90%" icon={<Icon.Home />} color="blue" />
-          <InfoCard title="Orders" value="2,000" icon={<Icon.Calendar />} color="red" />
-          <InfoCard title="New User" value="+300" icon={<Icon.Users />} color="green" />
-          <InfoCard title="User" value="10,000" icon={<Icon.Camera />} color="lime" />
-        </>
+        <InfoCard title="Income" value="90%" icon={<Icon.Home />} color="blue" />
+        <InfoCard title="Orders" value="2,000" icon={<Icon.Calendar />} color="red" />
+        <InfoCard title="New User" value="+300" icon={<Icon.Users />} color="green" />
+        <InfoCard title="User" value="10,000" icon={<Icon.Camera />} color="lime" />
       </InfoCardContainer>
-      <Card>
-        <h1>coba card component</h1>
-      </Card>
+      <BarChart />
     </>
   )
 }
