65956872f2851893fec9fd3af8f61355e289b3a8
mhaecal <haecal.dk@gmail.com>
2022-04-10 10:52:26 +0700

    chore: add `Breadcrumb` component

---
+++ src/components/Breadcrumb.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import classNames from 'classnames'
+
+type BreadcrumbProps = {
+  data: string[]
+}
+
+function Breadcrumb({ data }: BreadcrumbProps) {
+  return (
+    <div className="breadcrumb-list">
+      <ul className="flex items-center space-x-2 text-sm">
+        {data.map((name, index) => (
+          <li
+            key={index}
+            className={classNames({
+              'breadcrumb-item': index > 0,
+              active: index == data.length - 1,
+            })}
+          >
+            {name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Breadcrumb
