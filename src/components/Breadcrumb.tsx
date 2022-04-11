2553278c2ed69e4d75fe905a4ce5e05c302a9c17
mhaecal <haecal.dk@gmail.com>
2022-04-11 20:45:16 +0700

    chore(Breadcrumb): margin bottom

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -7,7 +7,7 @@ type BreadcrumbProps = {
 
 function Breadcrumb({ data }: BreadcrumbProps) {
   return (
-    <div className="breadcrumb-list">
+    <div className="breadcrumb-list mb-4">
       <ul className="flex items-center space-x-2 text-sm">
         {data.map((name, index) => (
           <li
