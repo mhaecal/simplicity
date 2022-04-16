b27aa0863d79f025c7546acd54a856e36e642139
mhaecal <haecal.dk@gmail.com>
2022-04-16 20:26:38 +0700

    chore(Input): add error message

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,19 +6,28 @@ type InputProps = {
   className?: string
   type?: 'text' | 'password' | 'email'
   onChange?: () => any
+  error?: string
 }
 
-function Input({ placeholder, className, type = 'text', onChange }: InputProps) {
+function Input({ placeholder, className, type = 'text', onChange, error }: InputProps) {
   return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      className={classNames(
-        'w-full relative place-self-end border border-gray-300 h-10 px-3 rounded focus:outline-none focus:ring focus:ring-blue-300',
-        className
+    <>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className={classNames(
+          'w-full relative place-self-end border border-gray-300 h-10 px-3 rounded focus:outline-none focus:ring focus:ring-blue-300',
+          className,
+          { 'border-red-400 focus:ring-red-100': error }
+        )}
+        onChange={onChange}
+      />
+      {error && (
+        <span className="text-xs text-red-700" id="passwordHelp">
+          {error}
+        </span>
       )}
-      onChange={onChange}
-    />
+    </>
   )
 }
 
