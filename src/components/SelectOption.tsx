e87c5b99263bd3d50fc704c88b2d00e8bfd3dd48
mhaecal <haecal.dk@gmail.com>
2022-04-16 20:27:09 +0700

    chore(SelectOption): add selected & disabled props

--- src/components/SelectOption.tsx
+++ src/components/SelectOption.tsx
@@ -3,10 +3,16 @@ import React, { ReactElement } from 'react'
 type SelectOptionProps = {
   value?: string | number
   children: string
+  selected?: boolean
+  disabled?: boolean
 }
 
-function SelectOption({ value, children }: SelectOptionProps) {
-  return <option value={value ? value : children}>{children}</option>
+function SelectOption({ value, children, selected = false, disabled = false }: SelectOptionProps) {
+  return (
+    <option value={value ? value : children} selected={selected} disabled={disabled}>
+      {children}
+    </option>
+  )
 }
 
 export default SelectOption
