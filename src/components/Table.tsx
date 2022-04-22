14179836d1428fde281b12829fb29ae0b846191a
mhaecal <haecal.dk@gmail.com>
2022-04-22 21:23:56 +0700

    chore(Table): className option

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -7,17 +7,20 @@ type TableProps = any
 
 function Table({ config }: TableProps) {
   const gridTable = useRef(null)
+  let grid = new Grid()
 
-  const grid = new Grid(
-    Object.assign(config, {
-      className: {
-        container: 'custom-table',
-        table: 'w-full',
-        th: 'bg-gray-100',
-        footer: 'shadow-none rounded',
-      },
-    })
-  )
+  config.className
+    ? (grid = new Grid(config))
+    : (grid = new Grid(
+        Object.assign(config, {
+          className: {
+            container: 'custom-table',
+            table: 'w-full',
+            th: 'bg-gray-100',
+            footer: 'shadow-none rounded',
+          },
+        })
+      ))
 
   useEffect(() => {
     gridTable.current && grid.render(gridTable.current)
